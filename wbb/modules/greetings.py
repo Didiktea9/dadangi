cfacce7ee445645bcc0efba0f1bf2289cdb5efc1
Didiktea <86914002+Didiktea9@users.noreply.github.com>
2021-11-03 21:00:50 +0530

    Update greetings.py

--- wbb/modules/greetings.py
+++ wbb/modules/greetings.py
@@ -128,9 +128,9 @@ async def welcome(_, message: Message):
 
             await message.chat.restrict_member(member.id, ChatPermissions())
             text = (
-                f"{(member.mention())} Mihring i nih ngei em?\n"
+                f"{(member.mention())} Mihring i ni ngei em?\n"
                 f"Thlalak a inziak khi a hnuai ami ah khuan {WELCOME_DELAY_KICK_SEC} "
-                "seconds chhung a i hmet hmanlo emaw vawi li i tidiklo anih chuan kick i ni ang."
+                "seconds chhung a i hmet hmanlo emaw vawi 4 i tidiklo anih chuan kick i ni ang."
             )
         except ChatAdminRequired:
             return
