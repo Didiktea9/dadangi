0a6fa6a82a738c5e50166cdebcbdca1c4a201d59
Didiktea <86914002+Didiktea9@users.noreply.github.com>
2021-11-06 00:25:37 +0530

    Update quotly.py

--- wbb/modules/quotly.py
+++ wbb/modules/quotly.py
@@ -32,9 +32,9 @@ from wbb.core.decorators.errors import capture_err
 
 __MODULE__ = "Quotly"
 __HELP__ = """
-/q - To quote a message.
-/q [INTEGER] - To quote more than 1 messages.
-/q r - to quote a message with it's reply
+➤/q - To quote a message.
+➤/q [INTEGER] - To quote more than 1 messages.
+➤/q r - to quote a message with it's reply
 
 Use .q to quote using userbot
 """
