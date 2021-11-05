faf209c7652f646806e55ff8dd9656d0f9f5b53f
Didiktea <86914002+Didiktea9@users.noreply.github.com>
2021-11-06 00:11:19 +0530

    Update karma.py

--- wbb/modules/karma.py
+++ wbb/modules/karma.py
@@ -38,7 +38,7 @@ from wbb.utils.functions import get_user_id_and_usernames
 __MODULE__ = "Karma"
 __HELP__ = """[UPVOTE] - Use upvote keywords like "+", "+1", "thanks" etc to upvote a message.
 [DOWNVOTE] - Use downvote keywords like "-", "-1", etc to downvote a message.
-/karma_toggle [ENABLE|DISABLE] - Enable or Disable Karma System In Your Chat.
+➤/karma_toggle [ENABLE|DISABLE] - Enable or Disable Karma System In Your Chat.
 Reply to a message with /karma to check a user's karma
 Send /karma without replying to any message to check karma list of top 10 users"""
 
