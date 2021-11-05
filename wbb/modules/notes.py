a5b0df24c9e1cf6433addd2a902ea0b0e6277ff3
Didiktea <86914002+Didiktea9@users.noreply.github.com>
2021-11-06 00:21:47 +0530

    Update notes.py

--- wbb/modules/notes.py
+++ wbb/modules/notes.py
@@ -34,13 +34,13 @@ from wbb.utils.dbfunctions import (delete_note, get_note, get_note_names,
 from wbb.utils.functions import extract_text_and_keyb
 
 __MODULE__ = "Notes"
-__HELP__ = """/notes To Get All The Notes In The Chat.
+__HELP__ = """➤/notes To Get All The Notes In The Chat.
 
-/save [NOTE_NAME] To Save A Note (Can be a sticker or text).
+➤/save [NOTE_NAME] To Save A Note (Can be a sticker or text).
 
 #NOTE_NAME To Get A Note.
 
-/delete [NOTE_NAME] To Delete A Note.
+➤/delete [NOTE_NAME] To Delete A Note.
 
 Checkout /markdownhelp to know more about formattings and other syntax.
 """
