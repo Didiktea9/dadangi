d9e54329051109e1a43b456858fe880630d45e30
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2021-11-08 21:08:01 +0530

    Update send.py

--- wbb/modules/send.py
+++ wbb/modules/send.py
@@ -1,7 +1,9 @@
 from pyrogram import filters
 from wbb import app
+from wbb.core.decorators.errors import capture_err
 
 @app.on_message(filters.command("snd"))
+@capture_err
 async def send(_, message):
   rsr = message.text.split(None, 1)[1]
   await app.send_message(message.chat.id, text=rsr, disable_web_page_preview=True)
