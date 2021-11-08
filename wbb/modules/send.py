831e1a466e970ae63b39dd1436b389260140f726
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2021-11-08 21:37:00 +0530

    Update send.py

--- wbb/modules/send.py
+++ wbb/modules/send.py
@@ -1,9 +1,8 @@
 from pyrogram import filters
 from wbb import app
-from wbb.core.decorators.permissions import adminsOnly
+
 
 @app.on_message(filters.command("snd"))
-@adminsOnly
 async def send(_, message):
   rsr = message.text.split(None, 1)[1]
   await app.send_message(message.chat.id, text=rsr, disable_web_page_preview=True)
