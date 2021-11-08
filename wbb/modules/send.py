117fe1e74c975c088cbd4d72792fc61bf944ae68
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2021-11-08 20:42:51 +0530

    Update send.py

--- wbb/modules/send.py
+++ wbb/modules/send.py
@@ -4,5 +4,5 @@ from wbb import app
 @app.on_message(filters.command("snd") & ~filters.edited)
 async def send(_, message):
   rsr = message.text.split(None, 1)[1]
-  await app.send_message(rsr, disable_web_page_preview=True)
+  await app.send_message(text=rsr, disable_web_page_preview=True)
   await message.delete()
