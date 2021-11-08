1122906d11f6c776c3cabe272b6c0ee5db0610db
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2021-11-08 20:23:06 +0530

    Update send.py

--- wbb/modules/send.py
+++ wbb/modules/send.py
@@ -1,2 +1,8 @@
-@app.on_message(filters.command("Lynn") & ~filters.edited)
-async def asq(_, message):
+@app.on_message(filters.command("snd") & ~filters.edited)
+async def send(_, message):
+  rsr = message.text.split(None, 1)[1]
+  if not rsr:
+    await message.reply_text("Thawn tur dah tel rawh")
+    return
+  await app.send_message(rsr)
+  await message.delete()
