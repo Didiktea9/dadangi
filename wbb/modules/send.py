463f00017edfe6a478ee792b937a03d579893afb
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2021-11-08 20:26:45 +0530

    Update send.py

--- wbb/modules/send.py
+++ wbb/modules/send.py
@@ -1,8 +1,11 @@
+from pyrogram import filters
+
+
 @app.on_message(filters.command("snd") & ~filters.edited)
 async def send(_, message):
   rsr = message.text.split(None, 1)[1]
   if not rsr:
     await message.reply_text("Thawn tur dah tel rawh")
     return
-  await app.send_message(rsr)
+  await app.send_message(rsr, disable_web_page_preview=True)
   await message.delete()
