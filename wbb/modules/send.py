91cdae5487765e34ad29128b1e6286391117ff75
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2021-11-08 20:43:52 +0530

    Update send.py

--- wbb/modules/send.py
+++ wbb/modules/send.py
@@ -1,7 +1,7 @@
 from pyrogram import filters
 from wbb import app
 
-@app.on_message(filters.command("snd") & ~filters.edited)
+@app.on_message(filters.command("snd"))
 async def send(_, message):
   rsr = message.text.split(None, 1)[1]
   await app.send_message(text=rsr, disable_web_page_preview=True)
