d743969e20d5878644ea1046cc066d1937539656
Didiktea <86914002+Didiktea9@users.noreply.github.com>
2021-11-09 22:53:04 +0530

    Update misc.py

--- wbb/utils/misc.py
+++ wbb/utils/misc.py
@@ -18,7 +18,9 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 from math import ceil
+
 from pyrogram.types import InlineKeyboardButton
+
 from wbb import MOD_LOAD, MOD_NOLOAD
 
 
@@ -40,7 +42,8 @@ def paginate_modules(page_n, module_dict, prefix, chat=None):
                 EqInlineKeyboardButton(
                     x.__MODULE__,
                     callback_data="{}_module({})".format(
-                        prefix, x.__MODULE__.lower()),
+                        prefix, x.__MODULE__.lower()
+                    ),
                 )
                 for x in module_dict.values()
             ]
@@ -58,72 +61,49 @@ def paginate_modules(page_n, module_dict, prefix, chat=None):
             ]
         )
 
-    pairs = list(zip(module[i * 4 : (i + 1) * 4] for i in range((len(modules) + 4 - 1) // 4)]
-
-    round_num = len(modules) / 4
-    calc = len(modules) - round(round_num)
-    if calc in [1, 2]:
+    pairs = list(zip(modules[::3], modules[1::3], modules[2::3]))
+    i = 0
+    for m in pairs:
+        for _ in m:
+            i += 1
+    if len(modules) - i == 1:
         pairs.append((modules[-1],))
-    elif calc == 2:
-        pairs.append((modules[-1],))
+    elif len(modules) - i == 2:
+        pairs.append(
+            (
+                modules[-2],
+                modules[-1],
+            )
+        )
 
-    else:
-        pairs += [[EqInlineKeyboardButton("「 GO BACK TO MAIN MENU 」", callback_data="help_back")]]
+    COLUMN_SIZE = 20
+
+    max_num_pages = ceil(len(pairs) / COLUMN_SIZE)
+    modulo_page = page_n % max_num_pages
+
+    # can only have a certain amount of buttons side by side
+    if len(pairs) > COLUMN_SIZE:
+        pairs = pairs[
+            modulo_page * COLUMN_SIZE : COLUMN_SIZE * (modulo_page + 1)
+        ] + [
+            (
+                EqInlineKeyboardButton(
+                    "❮",
+                    callback_data="{}_prev({})".format(prefix, modulo_page),
+                ),
+                EqInlineKeyboardButton(
+                    "Back",
+                    callback_data="{}_home({})".format(prefix, modulo_page),
+                ),
+                EqInlineKeyboardButton(
+                    "❯",
+                    callback_data="{}_next({})".format(prefix, modulo_page),
+                ),
+            )
+        ]
 
     return pairs
 
 
-def send_to_list(
-    bot: Bot, send_to: list, message: str, markdown=False, html=False
-) -> None:
-    if html and markdown:
-        raise Exception("Can only send with either markdown or HTML!")
-    for user_id in set(send_to):
-        try:
-            if markdown:
-                bot.send_message(user_id, message, parse_mode=ParseMode.MARKDOWN)
-            elif html:
-                bot.send_message(user_id, message, parse_mode=ParseMode.HTML)
-            else:
-                bot.send_message(user_id, message)
-        except TelegramError:
-            pass  # ignore users who fail
-
-
-def build_keyboard(buttons):
-    keyb = []
-    for btn in buttons:
-        if btn.same_line and keyb:
-            keyb[-1].append(InlineKeyboardButton(btn.name, url=btn.url))
-        else:
-            keyb.append([InlineKeyboardButton(btn.name, url=btn.url)])
-
-    return keyb
-
-
-def revert_buttons(buttons):
-    res = ""
-    for btn in buttons:
-        if btn.same_line:
-            res += "\n[{}](buttonurl://{}:same)".format(btn.name, btn.url)
-        else:
-            res += "\n[{}](buttonurl://{})".format(btn.name, btn.url)
-
-    return res
-
-
-def build_keyboard_parser(bot, chat_id, buttons):
-    keyb = []
-    for btn in buttons:
-        if btn.url == "{rules}":
-            btn.url = "http://t.me/{}?start={}".format(bot.username, chat_id)
-        if btn.same_line and keyb:
-            keyb[-1].append(InlineKeyboardButton(btn.name, url=btn.url))
-        else:
-            keyb.append([InlineKeyboardButton(btn.name, url=btn.url)])
-
-    return keyb
-
-
 def is_module_loaded(name):
-    return name not in NO_LOAD
+    return (not MOD_LOAD or name in MOD_LOAD) and name not in MOD_NOLOAD
