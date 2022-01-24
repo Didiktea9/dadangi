3ee46931ac6f9ebcac6eaac19d84c77ed2cfe39a
Didiktea <86914002+Didiktea9@users.noreply.github.com>
2022-01-24 10:57:12 +0530

    Update constants.py

--- wbb/utils/constants.py
+++ wbb/utils/constants.py
@@ -6,29 +6,29 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from wbb import BOT_USERNAME, app
 
 MARKDOWN = """
-Read the below text carefully to find out how formatting works!
+Format diktak a i hman theih nan a hnuai ami te khu chiang takin chhiar ang che!
 
-<u>Supported Fillings:</u>
+<u>Fillings hman theih te:</u>
 
-<code>{name}</code> - This will mention the user with their name.
-<code>{chat}</code> - This will fill with the current chat name.
+<code>{name}</code> - he format hi chuan user hming mention pahin alo tilang ang.
+<code>{chat}</code> - he format hi chu Group hming tihlan na ani ve thung.
 
-NOTE: Fillings only works in greetings module.
-
-
-<u>Supported formatting:</u>
-
-<code>**Bold**</code> : This will show as <b>bold</b> text.
-<code>~~strike~~</code>: This will show as <strike>strike</strike> text.
-<code>__italic__</code>: This will show as <i>italic</i> text.
-<code>--underline--</code>: This will show as <u>underline</u> text.
-<code>`code words`</code>: This will show as <code>code</code> text.
-<code>||spoiler||</code>: This will show as <spoiler>Spoiler</spoiler> text.
-<code>[hyperlink](google.com)</code>: This will create a <a href='https://www.google.com'>hyperlink</a> text.
-<b>Note:</b> You can use both markdown & html tags.
+NOTE: Greetings module a fillings hman theih te.
 
 
-<u>Button formatting:</u>
+<u>Format hman tur te:</u>
+
+<code>**Bold**</code> : Sawtiang saw <b>Hawrawppui</b> a thu ziahna.
+<code>~~strike~~</code>: Sawtiang saw <strike>Thaichhiat</strike> na ani.
+<code>__italic__</code>: Sawtiang saw <i>italic</i> a ziahna.
+<code>--underline--</code>: Sawtiang saw <u>underline</u> a thuziah na.
+<code>`code words`</code>: sawtiang saw thuziak <code>code</code> a ziahna.
+<code>||spoiler||</code>: Sawtiang saw <spoiler>Spoiler</spoiler> a thu ziahna.
+<code>[hyperlink](google.com)</code>: Sawtiang saw thuziak <a href='https://www.google.com'>hyperlink</a> te link a dahna.
+<b>Note:</b> Markdown & html tag te i hmang thei ve ve.
+
+
+<u>Button format ve thung:</u>
 
 -> text ~ [button text, button link]
 
@@ -45,7 +45,7 @@ async def mkdwnhelp(_, m: Message):
         [
             [
                 InlineKeyboardButton(
-                    text="Click Here!",
+                    text="Min hmet rawh!",
                     url=f"http://t.me/{BOT_USERNAME}?start=mkdwn_help",
                 )
             ]
@@ -53,7 +53,7 @@ async def mkdwnhelp(_, m: Message):
     )
     if m.chat.type != "private":
         await m.reply(
-            "Click on the below button to get markdown usage syntax in pm!",
+            "A hnuai a button khu hmet rawh!",
             reply_markup=keyb,
         )
     else:
